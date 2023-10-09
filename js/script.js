c651db0a7fc86c8ac303dca0e3553dd30d9ef466
Hycarus <mirkomarasco08@gmail.com>
2023-10-09 16:49:58 +0200

    refactory

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
-let kilometers;
-let age;
+let kilometers = parseInt(prompt('Quanti kilometri vuoi percorrere oggi?'));
+let age = parseInt(prompt('Quanti anni hai?'));
 const price = 0.21;
 let finalPrice = document.getElementById('prezzo');
 
 if(isNaN(age) || isNaN(kilometers)){
-    kilometers = parseInt(prompt('Quanti kilometri vuoi percorrere oggi?'));
-    age = parseInt(prompt('Quanti anni hai?'));
+    alert('Inserisci valori validi');
     window.location.reload();
 }
 let totalPrice = (price * kilometers);
 if(age > 65){
     totalPrice = (totalPrice - (totalPrice * 40 / 100)).toFixed(2);
-    finalPrice.innerHTML = totalPrice;
+    
 } else if(age < 18){
     totalPrice = (totalPrice - (totalPrice * 20 / 100)).toFixed(2);
-    finalPrice.innerHTML = totalPrice;
-} else{
-    finalPrice.innerHTML = totalPrice;
 }
+finalPrice.innerHTML = totalPrice + '\u20AC';
