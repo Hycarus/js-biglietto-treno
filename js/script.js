5b322d7fd4740b2e51988684fd13cbdf2597ec6c
Hycarus <mirkomarasco08@gmail.com>
2023-10-09 16:39:07 +0200

    added control number for age and kilometers

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
-let kilometers = parseInt(prompt('Quanti kilometri vuoi percorrere oggi?'));
-let age = parseInt(prompt('Quanti anni hai?'));
+let kilometers;
+let age;
 const price = 0.21;
-let totalPrice = (price * kilometers);
 let finalPrice = document.getElementById('prezzo');
 
+if(isNaN(age) || isNaN(kilometers)){
+    kilometers = parseInt(prompt('Quanti kilometri vuoi percorrere oggi?'));
+    age = parseInt(prompt('Quanti anni hai?'));
+    window.location.reload();
+}
+let totalPrice = (price * kilometers);
 if(age > 65){
     totalPrice = (totalPrice - (totalPrice * 40 / 100)).toFixed(2);
     finalPrice.innerHTML = totalPrice;
