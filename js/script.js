d258955ee197f281459fd4178340a2239d608b51
Hycarus <mirkomarasco08@gmail.com>
2023-10-09 16:31:31 +0200

    added 2 cifre decimali

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ let totalPrice = (price * kilometers);
 let finalPrice = document.getElementById('prezzo');
 
 if(age > 65){
-    totalPrice = totalPrice - (totalPrice * 40 / 100);
+    totalPrice = (totalPrice - (totalPrice * 40 / 100)).toFixed(2);
     finalPrice.innerHTML = totalPrice;
 } else if(age < 18){
-    totalPrice = totalPrice - (totalPrice * 20 / 100);
+    totalPrice = (totalPrice - (totalPrice * 20 / 100)).toFixed(2);
     finalPrice.innerHTML = totalPrice;
 } else{
     finalPrice.innerHTML = totalPrice;
